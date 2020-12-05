622477c145e4df78ec60712d3b95604ce13a9f12
D-Bhatta <dbhatta1232@gmail.com>
2020-12-05 16:27:47 -0500

    feat(views): Add view to render dummy page
    Add a view that renders the dummy template.
    
    It will be useful during development.

--- django_invoicing/django_invoicing/views.py
+++ django_invoicing/django_invoicing/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from django_apps.utils import get_logger
+
+lg = get_logger()
 
 # Create your views here.
 
 
 def homepage(request):
+    lg.debug("Rendering homepage")
     return render(request, "homepage.html", {})
+
+
+def dummy(request):
+    lg.debug("Rendering dummypage")
+    return render(request, "dummy.html", {})
