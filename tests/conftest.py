fe72a0b33c000deff5845ab76ef7397cd5d5d225
D-Bhatta <dbhatta1232@gmail.com>
2020-12-11 09:28:03 -0500

    ref(test): Add loading test env vars
    Add loading of test environment variables in pytest configuration.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,14 @@
-from os import remove
+from os import path, remove
+from pathlib import Path
 from shutil import copy, rmtree
+
+import dotenv
+from django_apps.utils import get_logger
+
+lg = get_logger()
+
+# Build paths inside the project like this: PROJ_DIR / 'subdir'.
+PROJ_DIR = Path(__file__).resolve().parent.parent
 
 
 def pytest_configure():
@@ -7,6 +16,11 @@ def pytest_configure():
         "django_invoicing/django_apps/templates/base.html",
         "django_invoicing/django_invoicing/templates/",
     )
+    try:
+        path_env = path.join(PROJ_DIR, "env\\test.env")
+        dotenv.read_dotenv(path_env)
+    except (EnvironmentError, FileNotFoundError):
+        print("Couldn't retrieve the environment variables")
 
 
 def pytest_unconfigure():
