cafde12bf08627aad8bd677a7a883b3abe126bf8
D-Bhatta <dbhatta1232@gmail.com>
2020-12-12 05:28:25 -0500

    feat(test): Add log in fixture
    Add a fixture to log in an user

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -9,3 +9,14 @@ def create_user(db) -> User:
     USERNAME = environ["USERS_LOGIN_USERNAME"]
     PASSWORD = environ["USERS_LOGIN_PASSWORD"]
     return User.objects.create_user(username=USERNAME, password=PASSWORD)
+
+
+@pytest.fixture
+def logged_in(db, client, create_user):
+    USERNAME = environ["USERS_LOGIN_USERNAME"]
+    PASSWORD = environ["USERS_LOGIN_PASSWORD"]
+
+    # Log client in and check login
+    client.login(username=USERNAME, password=PASSWORD)
+
+    return client
