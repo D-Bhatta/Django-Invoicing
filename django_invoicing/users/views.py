48cd46a2a0659603439b0bc94cc2ed79caeb7ba3
D-Bhatta <dbhatta1232@gmail.com>
2021-01-05 13:43:48 -0500

    ref(usr-view): Create register view

--- django_invoicing/users/views.py
+++ django_invoicing/users/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import Http404, redirect, render
+from django.urls import reverse
+from django_apps.utils import get_logger
+from users.forms import NewUserCreationForm
+
+lg = get_logger()
 
 # Create your views here.
+
+
+def register(request):
+    lg.debug("Rendering register page")
+    if request.method == "GET":
+        return render(request, "register.html", {"form": NewUserCreationForm})
+    elif request.method == "POST":
+        form = NewUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect(reverse("invoicing:home"))
